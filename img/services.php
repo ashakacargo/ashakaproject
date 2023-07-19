5f0afeb379130a7cfbd587e93a257a13cb239cee
ashakacargo <139955019+ashakacargo@users.noreply.github.com>
2023-07-19 15:05:31 +0700

    Add files via upload

---
+++ img/services.php
@@ -0,0 +1,76 @@
+    <!-- ======= Services Section ======= -->
+    <section id="service" class="services pt-0">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <span>Our Services</span>
+          <h2>Our Services</h2>
+
+        </div>
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="card">
+              <div class="card-img">
+                <img src="<?php echo base_url('../../img/trucking-service-3.jpg') ?>" alt="" class="img-fluid">
+              </div>
+              <h3><a href="<?= base_url('../../darat') ?>" class="stretched-link">Pengiriman Darat</a></h3>
+              <!-- <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p> -->
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="card">
+              <div class="card-img">
+                <img src="<?php echo base_url('../../img/features-4.jpg') ?>" alt="" class="img-fluid">
+              </div>
+              <h3><a href="<?= base_url('../../laut') ?>" class="stretched-link">Pengiriman Laut</a></h3>
+              <!-- <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p> -->
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="card">
+              <div class="card-img">
+                <img src="<?php echo base_url('../../img/cargo-service-2.jpg') ?>" alt="" class="img-fluid">
+              </div>
+              <h3><a href="<?= base_url('../../udara') ?>" class="stretched-link">Pengiriman Udara</a></h3>
+              <!-- <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p> -->
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="card">
+              <div class="card-img">
+                <img src="<?php echo base_url('../../img/sewa-truck-4.jpg') ?>" alt="" class="img-fluid">
+              </div>
+              <h3><a href="<?= base_url('../../sewatruck') ?>" class="stretched-link">Sewa Truck</a></h3>
+              <!-- <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p> -->
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="card">
+              <div class="card-img">
+                <img src="<?php echo base_url('../../img/jasa-pindahan.jpg') ?>" alt="" class="img-fluid">
+              </div>
+              <h3><a href="<?= base_url('../../jasapindahan') ?>" class="stretched-link">Jasa Pindahan</a></h3>
+              <!-- <p>Cumque eos in qui numquam. Aut aspernatur perferendis sed atque quia voluptas quisquam repellendus temporibus itaqueofficiis odit</p> -->
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="card">
+              <div class="card-img">
+                <img src="<?php echo base_url('../../img/jasa-kirim.jpg') ?>" alt="" class="img-fluid">
+              </div>
+              <h3><a href="<?= base_url('../../motor') ?>" class="stretched-link">Jasa Kirim Motor, Mobil, dan Alat Berat</a></h3>
+              <!-- <p>Asperiores provident dolor accusamus pariatur dolore nam id audantium ut et iure incidunt molestiae dolor ipsam ducimus occaecati nisi</p> -->
+            </div>
+          </div><!-- End Card Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
